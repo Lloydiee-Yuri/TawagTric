98f7e16ff6a4b85d5e5962d5a3cc842fe55ee162
Jhon Lloyd <jhonloreto1@gmail.com>
2024-10-31 16:43:39 +0800

    kenet test3

---
+++ constants/images.js
@@ -0,0 +1,14 @@
+import profile from "../assets/images/profile.png";
+import thumbnail from "../assets/images/thumbnail.png";
+import cards from "../assets/images/cards.png";
+import path from "../assets/images/path.png";
+import logo from "../assets/images/logo.png";
+import logoSmall from "../assets/images/logo-small.png";
+import empty from "../assets/images/empty.png";
+import TawagLogo from "../assets/images/TawagTricLogo.png"
+import TawagLogoS from "../assets/images/TawagTricLogoSmall.png"
+import TawagLogoR from"../assets/images/TawagTricLogoRow.png"
+import tricycle from "../assets/images/tricycle.png"
+import JLB from "../assets/images/JLnBIRD.png"
+
+export default { profile, thumbnail, cards, path, logo, logoSmall, empty, TawagLogo, TawagLogoS, TawagLogoR, tricycle, JLB };
